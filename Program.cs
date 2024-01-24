d2f5d9efeb970acb6abfd55d208c5e224aae4222
MoFerraz <mo_ferraz@hotmail.com>
2024-01-24 19:11:17 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace tabuada2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 0;
+            
+            Console.WriteLine("Qual tabuada você deseja calcular? ");
+            int tabuada = int.Parse(Console.ReadLine());
+
+            while (numero <= 10)
+            {
+                int resultado = tabuada * numero;
+                Console.WriteLine($"{tabuada} x {numero} = {resultado} ");
+                numero++;
+                Thread.Sleep(500);
+            }
+           
+
+
+        }
+    }
+}
